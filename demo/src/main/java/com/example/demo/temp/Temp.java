1f654f7c0d7c947f8af0646da716b79afcadfb2b
hipopotamus <qkqntjddn@gmail.com>
2023-07-23 19:59:23 +0900

    clear

--- demo/src/main/java/com/example/demo/temp/Temp.java
+++ demo/src/main/java/com/example/demo/temp/Temp.java
@@ -2,11 +2,4 @@ package com.example.demo.temp;
 
 public class Temp {
     private int number;
-    private int temptation;
-    private int ex;
-    private int ox;
-    private int reset;
-    private int rereset;
-    private int rebase;
-    private int rerebase;
 }
